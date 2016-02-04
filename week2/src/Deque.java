9bdfc66fe84ecd41b92c58cb146ac7be726b7670
ekis <ekis@users.noreply.github.com>
2016-02-03 20:34:59 -0700

    ATE-2 Week 2 (Stacks, Queues, Sorts)
    Added stub classes.

---
+++ week2/src/Deque.java
@@ -0,0 +1,48 @@
+import java.util.Iterator;
+
+public class Deque<T> implements Iterable<T> {
+    // construct an empty deque
+    public Deque() {
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    // add the item to the front
+    public void addFirst(T item) {
+        throw new UnsupportedOperationException();
+    }
+
+    // add the item to the end
+    public void addLast(T item) {
+        throw new UnsupportedOperationException();
+    }
+
+    // remove and return the item from the front
+    public T removeFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    // remove and return the item from the end
+    public T removeLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    // return an iterator over items in order from front to end
+    @Override
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    // unit testing
+    public static void main(String[] args) {
+
+    }
+}
---
+++ week2/src/RandomizedQueue.java
@@ -0,0 +1,44 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<T> implements Iterable<T> {
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+
+    }
+
+    // is the queue empty?
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    // return the number of items on the queue
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    // add the item
+    public void enqueue(T item) {
+        throw new UnsupportedOperationException();
+    }
+
+    // remove and return a random item
+    public T dequeue() {
+        throw new UnsupportedOperationException();
+    }
+
+    // return (but do not remove) a random item
+    public T sample() {
+        throw new UnsupportedOperationException();
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    // unit testing
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+}
