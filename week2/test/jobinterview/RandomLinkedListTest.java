4f5d5575e20cff5fded370f1231408d1f0cc5042
ekis <ekis@users.noreply.github.com>
2017-02-03 14:54:40 -0700

    ATE-2 Week 2 (Stacks, Queues, Sorts)
    Job Interview 4:
    Clone linked structure with random pointer
    
    First take at implementation - works well for list size < ~115,
    then it suddenly starts to take > 30 seconds (exact time unknown
    as I'd stop the execution after that time).
    
    Can't spot quadratic behavior in the cloning - conjecture: the
    list start to form cycles and the code sits in this cycle indefintely.
    
    I may revisit this in the future.

---
+++ week2/test/jobinterview/RandomLinkedListTest.java
@@ -0,0 +1,39 @@
+package jobinterview;
+
+import jobinterview.linkedlist.RandomLinkedList;
+import org.junit.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ekis on 31/01/17.
+ */
+public class RandomLinkedListTest {
+
+    @Test
+    public void test() {
+        RandomLinkedList<Integer> linkedList = new RandomLinkedList<>();
+        assertTrue(linkedList.isEmpty());
+
+        for (int i = 0; i < 115; i++) linkedList.add(i);
+
+        assertFalse(linkedList.isEmpty());
+        assertEquals(115, linkedList.size());
+
+        String original = stringTraceOf(linkedList);
+        //System.out.println("Original list -> \n" + original);
+
+        RandomLinkedList<Integer> clonedList = linkedList.cloneLinkedList();
+        String cloned = stringTraceOf(clonedList);
+        //System.out.println("Cloned list -> \n" + cloned);
+        assertEquals(original, cloned);
+    }
+
+    private String stringTraceOf(RandomLinkedList<Integer> list) {
+        return StreamSupport.stream(list.spliterator(), false)
+                .collect(Collectors.joining("\n"));
+    }
+}
