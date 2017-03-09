d3089aab088f5020cd4c7d4a4cfc1479401e98f0
ekis <ekis@users.noreply.github.com>
2017-03-09 15:41:52 -0700

    ATE-4 Week 4 (Priority Queues, Heapsort)
    - heapsort now correctly operates with large data sets
    -- removed sortdown phase recursion and replaced it with simple loop
    - we are now working directly on the array without any 1-based conversion
    -- that saves 2 linear array copies: 0-based -> 1-based -> sort -> 0-based
    - sort tests are also improved so that random data tests are included in
    tabular representation

--- week3/src/jobinterview/sort/heapsort/MyHeapsort.java
+++ week3/src/jobinterview/sort/heapsort/MyHeapsort.java
@@ -1,83 +1,67 @@
 package jobinterview.sort.heapsort;
 
-import java.util.Arrays;
-import java.util.function.BiConsumer;
-
-import static jobinterview.SortUtility.*;
+import static jobinterview.SortUtility.exch;
 
 /**
- * Implementation of heapsort works in two phases:
- * 1) Heap construction - we reorganise the input array into a heap (NlgN)
- * 2) Sortdown - we pull the items off of the heap in decreasing order
+ * This implementation of heapsort uses a 0-based priority queue, so as to prevent expensive conversions of array into
+ * 1-based pre-sorting and back into 0-based post-sorting (essentially, calls to System.arraycopy())
+ *
+ * The algorithm works in two phases:
+ * 1) Heap construction - we reorganise the input array into a heap, starting half-way (skipping one-sized heaps)
+ * through array and creating binary heap top-down (time complexity - 0(1/2 NlgN)
+ * 2) Sortdown - we exchange the first element in the array (i.e. heap max) and the last unsorted element and then
+ * simply sink down the unsorted element back into its place that preserves the binary heap invariant
+ * (time-complexity - O(NlgN)).
+ * Total time complexity is thus O(3/2 NlgN).
+ *
+ * It is very compact and performs equally well as the fastest quicksort implementation here - however is not generally
+ * used as it suffers from poor cache performance (neighbouring array entries are rarely compared so it suffers more
+ * cache misses than QS)
+ *
  */
 public final class MyHeapsort {
 
     public static <T extends Comparable<? super T>> void sort(T[] a) {
-        //T[] copy = copyArray(a, (input, sorted) -> System.arraycopy(input, 0, sorted, 1, input.length));
-        T[] copy = copyArray(a);
-        int M = copy.length;
-        int mid = M / 2; // anything to the right of this are just one-node heaps we needn't sort in Phase 1
-        for (int i = mid; i > 0; i--) { // phase 1 - construct heap
-            sink(copy, i, M - 1);
-        }
-        sort(copy, M - 1);
-        System.arraycopy(copy, 1, a, 0, a.length);
-    }
-
-    private static <T extends Comparable<? super T>> T[] copyArray(T[] a) {
         int N = a.length;
-        @SuppressWarnings("unchecked") // no better way to create a generic array
-        T[] sorted = (T[]) new Comparable[N + 1]; // 1-based array
-        System.arraycopy(a, 0, sorted, 1, a.length);
-        return sorted;
+        int mid = N / 2; // anything to the right of this are just one-node heaps we needn't sort in Phase 1
+        for (int i = mid; i >= 0; i--) // phase 1 - construct heap
+            sink(a, i, N - 1);
+        sort(a, N - 1); // phase 2 - sortdown
     }
 
-    /**
-     * Restores heap order by swimming up the node to its correct place bottom-up.
-     */
-    private static <T extends Comparable<? super T>> void swim(T[] xs, int k) {
-        int parent = k / 2;
-        if (parent >= 1 && less(xs, parent, k)) { // is parent < k
-            exch(xs, parent, k);
-            swim(xs, parent);
+    // N is sink limit - the array index of the first element that maintains the sort invariant
+    // this value will start at the array end and progressively shrink until the array is fully sorted
+    private static <T extends Comparable<? super T>> void sort(T[] xs, int N) {
+        while (N != 0) {
+            exch(xs, N--, 0);
+            sink(xs, 0, N);
         }
     }
 
     /**
      * Restores heap order by sinking the node to its correct place top-down.
      */
-    private static <T extends Comparable<? super T>> void sink(T[] xs, int k, int size) {
+    private static <T extends Comparable<? super T>> void sink(T[] xs, int k, int limit) {
         int left = 2 * k;
         int right = left + 1;
-        if (left > size) {
-            if (right > size) return; // is leaf, nothing to do but return
+        if (left > limit) {
+            if (right > limit) return; // is leaf, nothing to do but return
             if (less(xs, k, right)) { // no left, k < right => sink k
                 exch(xs, k, right);
-                sink(xs, right, size);
+                sink(xs, left, limit);
             }
-        } else if (right > size) {
+        } else if (right > limit) {
             if (less(xs, k, left)) { // no right, k < left => sink k
                 exch(xs, k, left);
-                sink(xs, left, size);
+                sink(xs, right, limit);
             }
         } else { // both nodes are here, compare them, take larger and exchange with k-th node if k < larger
             int larger = less(xs, left, right) ? right : left;
             if (less(xs, k, larger)) {
                 exch(xs, k, larger);
-                sink(xs, larger, size);
+                sink(xs, larger, limit);
             }
         }
-        // stop sinking, array is heap-ordered
-    }
-
-    // N represents sink limit - the array index of the first element that maintains the sort invariant
-    // this value will start at the array end and progressively shrink until the array is fully sorted
-    private static <T extends Comparable<? super T>> void sort(T[] xs, int N) {
-        if (N == 1) return;
-        exch(xs, N--, 1);
-        sink(xs, 1, N);
-        //System.out.println("Sort stage -> " + Arrays.toString(xs));
-        sort(xs, N);
     }
 
     private static <T extends Comparable<? super T>> boolean less(T[] a, int i, int j) {
--- week3/test/jobinterview/sort/SortAlgorithmTest.java
+++ week3/test/jobinterview/sort/SortAlgorithmTest.java
@@ -71,7 +71,9 @@ public final class SortAlgorithmTest {
                 "       [S, O, R, T, E, X, A, M, P, L, E]         |        [A, E, E, L, M, O, P, R, S, T, X]         |        [A, E, E, L, M, O, P, R, S, T, X]         |    true   ", //
                 "[M, E, R, G, E, S, O, R, T, E, X, A, M, P, L, E] | [A, E, E, E, E, G, L, M, M, O, P, R, R, S, T, X] | [A, E, E, E, E, G, L, M, M, O, P, R, R, S, T, X] |    true   ", //
                 "[Q, U, I, C, K, S, O, R, T, E, X, A, M, P, L, E] | [A, C, E, E, I, K, L, M, O, P, Q, R, S, T, U, X] | [A, C, E, E, I, K, L, M, O, P, Q, R, S, T, U, X] |    true   ", //
-                "   [P, A, B, X, W, P, P, V, P, D, P, C, Y, Z]    |    [A, B, C, D, P, P, P, P, P, V, W, X, Y, Z]    |    [A, B, C, D, P, P, P, P, P, V, W, X, Y, Z]    |    true   " //
+                "   [P, A, B, X, W, P, P, V, P, D, P, C, Y, Z]    |    [A, B, C, D, P, P, P, P, P, V, W, X, Y, Z]    |    [A, B, C, D, P, P, P, P, P, V, W, X, Y, Z]    |    true   ", //
+                "          Uniformly random data input            |        Sorted uniformly random data input        |        Sorted uniformly random data input        |    true   ", //
+                "         Heavily duplicated data input           |                Sorted data input                 |                Sorted data input                 |    true   " //
         };
 
         StringGrid grid = grid("Input", "Expected", "Actual", "Is sorted?");
@@ -80,16 +82,15 @@ public final class SortAlgorithmTest {
         populateGridRow(grid, EXAMPLE_2, EXPECTED_2, SortTestSupport::copyExample2, algorithm);
         populateGridRow(grid, EXAMPLE_3, EXPECTED_3, SortTestSupport::copyExample3, algorithm);
         populateGridRow(grid, SEDGEWICK_DEMO_EXAMPLE, SEDGEWICK_DEMO_EXPECTED, SortTestSupport::copySedgewickExample, algorithm);
+        grid.row("Uniformly random data input", "Sorted uniformly random data input", "Sorted uniformly random data input", testRandomSort(algorithm, data.randoms()));
+        grid.row("Heavily duplicated data input", "Sorted data input", "Sorted data input", testRandomSort(algorithm, data.randoms()));
 
         TestSupport.check(grid, expectedGrid);
-
-        //testRandomSort(algorithm, data.randoms()); // test uniformly random data input
-        //testRandomSort(algorithm, data.duplicates()); // test heavily duplicated data input
     }
 
-    private static <T extends Comparable<? super T>> void testRandomSort(SortAlgorithm algorithm, T[] values) {
+    private static <T extends Comparable<? super T>> boolean testRandomSort(SortAlgorithm algorithm, T[] values) {
         algorithm.sort(values);
-        Assert.assertTrue(SortUtility.isSorted(values));
+        return SortUtility.isSorted(values);
     }
 
     private static void populateGridRow(StringGrid grid, String[] example, String[] expected, Supplier<String[]> actualSupplier, SortAlgorithm algorithm) {
